1e99f349508256f1ebd4a9075c9dfcb847eea04e
Brianmwaura <brianmwaura83@gmail.com>
2017-03-29 13:11:36 +0300

    my first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+	<title>Home</title>	
+	<link rel="stylesheet" href="css/style3.css"/>
+	<body style="background-color:lightgrey;">
+	</head>
+	<body>	
+		
+		
+ <ul class="w3-navbar w3-border w3-light-black">
+  <li><a class="w3-hover-red" href="index.php">Home</a></li>
+  <li><a class="w3-hover-blue" href="about.php">About Us</a></li>
+  <li><a class="w3-hover-green" href="gallery.php">Gallery</a></li>
+  <li><a class="w3-hover-teal" href="contact.php">Contact Us</a></li>
+</ul>
+<img class="w3-animate-zoom"    src="images/logo.jpg" ><br></br>
+
+	 <div class="w3-container w3-grey">
+ 
+	<img src="images/slide1.jpg " class="w3-round" alt="Slide1" width="100%">
+	<p>We welcome you to <b>Golden Palace Hotel.</b>We are located in Kenya, Kilifi County.
+		The hotel was completed in the year 2012 and officially opened on 28/01/2012.
+		It was designed by Architecture Peter Karanja and built by Biko builders and insuarance company.</p>
+	<p>We offer Resturant Services,Camp Services,Lodging Services,Cocktail,Recreational Services & Outside Catering.
+		We also have conference halls.
+		We offer 6-course meals and also a variety of meals in our Menu, both local and foreign dishes.	
+		We also have play-grounds where sporting activities can take place such as:Basketball, Football& Mini golf course.
+		We also have indoor sporting such as Table-Tennis</p>
+		
+	<p>Our Hotel is managed by Dennis insuarance company who offer training to Chefs,Waiters & Security guards and also provide Security Equipment.
+	</p>
+	<b>BENEFITS</b>
+	<p>We offer 10 free nights to our esteemed lucky customers every year.</p>
+
+	
+	
+	<footer class="w3-container w3-light-black">
+  
+  <p><b>&copy; copyright Developed & Designed By Brian.G.Mwaura (Biko) 2017</b></P>
+</footer> 
+	
+	</div>
+	
+
+
+	</body>
+</html>
